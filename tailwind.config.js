e03112e1699ebfd7cc949ca470b35f1942ceb33f
Pratik280 <pratikchandlekar280@gmail.com>
2022-01-16 07:16:12 +0530

    initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./dist/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // text primary
+        'very-dark-desaturated-blue': 'hsl(238, 29%, 16%)',
+        'soft-red': 'hsl(14, 88%, 65%)',
+        // gradient
+        'soft-violet': 'hsl(273, 12%, 33%)',
+        'soft-blue': 'hsl(240, 73%, 65%)',
+        // text neutral
+        'very-dark-grayish-blue': 'hsl(237, 12%, 33%)',
+        'dark-grayish-blue': 'hsl(240, 6%, 50%)',
+        // dividers
+        'light-grayish-blue': 'hsl(240, 5%, 91%)',
+      }
+    },
   },
   plugins: [],
 }
